e30d350ead64465575f8eb6accc3983926d71a3c
busaev <a@busaev.ru>
2020-01-31 13:49:29 +0300

    teamcity

--- postgresql/init.sh
+++ postgresql/init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
+
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     create database $GITEA_DATABASE;
     create user $GITEA_USER with encrypted password '$GITEA_PASSWORD';
     grant all privileges on database $GITEA_DATABASE to $GITEA_USER;
 
-    create database teamcity;
-    create user teamcity with encrypted password 'teamcity';
-    grant all privileges on database teamcity to teamcity;
+    create database $TEAMCITY_DATABASE;
+    create user $TEAMCITY_USER with encrypted password '$TEAMCITY_PASSWORD';
+    grant all privileges on database $TEAMCITY_DATABASE to $TEAMCITY_USER;
 EOSQL
